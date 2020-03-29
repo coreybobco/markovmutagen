50b5659d76932e761f380a67dcb8575a89ca1423
Corey Bobco <corey.bobco@gmail.com>
2020-03-29 12:16:55 -0500

    added output format to HTML and added aphorism format

--- server.py
+++ server.py
@@ -2,25 +2,7 @@
 from flask import Flask, request, render_template
 from generativepoetry.decomposer import cutup as cutup_technique
 from generativepoetry.decomposer import markov as markov_technique
-
-def clean(source_text):
-  # fix punctuation
-  print(source_text)
-  sentence_delimiters = [".", "?", "!"]
-  clause_delimiters = ["...", ";", "--"]
-  source_text = source_text.replace("---","--").replace("..", "...").replace("....", "...")
-  for char in (sentence_delimiters + clause_delimiters):
-    source_text = source_text.replace(char, char + " ")
-  # strip useless characters
-  source_text = source_text.replace("\n", " ")
-  if format == 'chat':
-    useless = ["\t", "\"", "\'"]
-  else:
-    useless = ["\t", "\n", "\""]
-  for char in useless:
-    source_text = source_text.replace(char, "")
-  word_list = source_text.split(" ")
-  return " ".join(word_list)
+from formatter import *
 
 app = Flask(__name__)
 
@@ -32,16 +14,23 @@ def index():
 def markov():
     cleaned_input = clean(request.get_data().decode(encoding='UTF-8'))
     ngram_size = int(request.args.get('ngram_size'))
-    return " ".join(markov_technique(cleaned_input, ngram_size=ngram_size))
+    output_format = request.args.get('output_format')
+    output = " ".join(markov_technique(cleaned_input, ngram_size=ngram_size))
+    if output_format == 'aphorisms':
+        output = format_aphorisms(output)
+    return output
+
 
 @app.route("/cutup", methods=['POST'])
 def cutup():
     cleaned_input = clean(request.get_data().decode(encoding='UTF-8'))
     cutup_min_size =  int(request.args.get('cutupmin'))
     cutup_max_size =  int(request.args.get('cutupmax'))
-    if cutup_min_size:
-        return " ".join(cutup_technique(cleaned_input, min_cutout_words=cutup_min_size,
-                                        max_cutout_words=cutup_max_size))
+    output = " ".join(cutup_technique(cleaned_input, min_cutout_words=cutup_min_size, max_cutout_words=cutup_max_size))
+    output_format = request.args.get('output_format')
+    if output_format == 'aphorisms':
+        output = format_aphorisms(output)
+    return output
 
 if __name__ == '__main__':
     app.run(
